19214115b4bb70f1cdf5ea5710db772ad970f946
jkimball54 <jeremyck54@gmail.com>
2023-08-07 10:56:47 -0600

    Test Project Set up

---
+++ MvcMessageLogger.Testing/MvcMessageLogger.Testing/UsersControllerTests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using MvcMessageLogger.DataAccess;
+using MvcMessageLogger.Models;
+
+namespace MvcMessageLogger.Testing
+{
+    public class UsersControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public UsersControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        private MvcMessageLoggerContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MvcMessageLoggerContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+
+            var context = new MvcMessageLoggerContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
