04b1f7ce344a2ecb74a3a50efb549571d48a0741
jkimball54 <jeremyck54@gmail.com>
2023-08-08 12:04:01 -0600

    2

--- MvcMessageLogger/Controllers/StatisticsController.cs
+++ MvcMessageLogger/Controllers/StatisticsController.cs
@@ -21,37 +21,10 @@ namespace MvcMessageLogger.Controllers
             var users = _context.Users.Include(u => u.Messages);
             ViewData["UsersOrderedByMessageCount"] = UsersOrderedByMessageCount(_context);
             ViewData["MostCommonWord"] = MostCommonWord(_context, 0);
-            ViewData["HourOfMostMessages"] = Hourlytwo(_context);
+            ViewData["HourOfMostMessages"] = HourOfMostMessages(_context);
             return View(users);
         }
 
-
-        public static string HourOfMostMessages(MvcMessageLoggerContext context)
-        {
-            //count messages that occur between range of time (1 hour)
-            //move through each hour from first occurance to last occurence
-            var finalHour = context.Messages.Max(m => m.CreatedAt.Hour) + 1;
-            string builtString = null;
-            for (var firstHour = context.Messages.Min(m => m.CreatedAt.Hour); firstHour < finalHour; firstHour++)
-            {
-                var currentHour = firstHour;
-                var nextHour = firstHour + 1;
-                var totalMessages = context.Messages.Where(m => m.CreatedAt.Hour < nextHour && m.CreatedAt.Hour >= currentHour).Count();
-                builtString += $"{TwelveHourTime(currentHour)}: {totalMessages}\n";
-            }
-            return builtString;
-        }
-
-        public static string TwelveHourTime(int hour)
-        {
-            return
-                (
-                hour < 12 ? $"{hour} AM" :
-                hour == 12 ? $"{hour} PM" :
-                hour == 0 ? $"{hour + 12} AM" :
-                $"{hour - 12} PM"
-                );
-        }
 
         public static string MostCommonWord(MvcMessageLoggerContext context, int minUsageNum)
         {
@@ -89,33 +62,20 @@ namespace MvcMessageLogger.Controllers
             return sortedDict;
         }
 
-        public static Dictionary<int, string> Hourly(MvcMessageLoggerContext context)
-        {
-            Dictionary<int, string> timeMessageCount = new Dictionary<int, string>();
-            var messageList = context.Messages;
-            var sortedList = messageList
-                .GroupBy
-                (m => m.CreatedAt.ToLocalTime().Hour).ToDictionary(group => group.Key, group => group.First());
-            foreach(var kv in sortedList)
-            {
-                timeMessageCount.Add(Convert.ToInt32(kv.Key), kv.Value.CreatedAt.ToLocalTime().ToString("h tt"));
-            }
-            return timeMessageCount;
-
-        }
-        public static Dictionary<string, int> Hourlytwo(MvcMessageLoggerContext context)
+ 
+        public static Dictionary<string, int> HourOfMostMessages(MvcMessageLoggerContext context)
         {
             Dictionary<string, int> timeMessageCount = new Dictionary<string, int>();
             var messageList = context.Messages;
             //initialize key value pairs
             foreach (var message in messageList)
             {
-                if(!timeMessageCount.ContainsKey(message.CreatedAt.ToLocalTime().ToString("h tt")))
+                if (!timeMessageCount.ContainsKey(message.CreatedAt.ToLocalTime().ToString("h tt")))
                 {
                     timeMessageCount.Add(message.CreatedAt.ToLocalTime().ToString("h tt"), 0);
                 }
             }
-            //currently working on messages not counting correctly, times are shown correctly but the value isnt being added up (ie 11am show only 1 message create when there is actually 3 messages at 11am) 
+            //Where key matches add to count of value for that hour
             foreach(var kvp in timeMessageCount)
             {
                 foreach(var message in messageList)
@@ -145,5 +105,30 @@ namespace MvcMessageLogger.Controllers
 //[]Most commonly used word for messages (by user and overall)
 // -
 
-//[]The hour with the most messages
+//[X]The hour with the most messages
 // - convert universal time to local, create ranges in hour increments, count messages for that range
+
+
+
+
+
+
+
+
+
+
+//Previous attempt at counting messages by hour
+//public static Dictionary<int, string> Hourly(MvcMessageLoggerContext context)
+//{
+//    Dictionary<int, string> timeMessageCount = new Dictionary<int, string>();
+//    var messageList = context.Messages;
+//    var sortedList = messageList
+//        .GroupBy
+//        (m => m.CreatedAt.ToLocalTime().Hour).ToDictionary(group => group.Key, group => group.First());
+//    foreach(var kv in sortedList)
+//    {
+//        timeMessageCount.Add(Convert.ToInt32(kv.Key), kv.Value.CreatedAt.ToLocalTime().ToString("h tt"));
+//    }
+//    return timeMessageCount;
+
+//}
